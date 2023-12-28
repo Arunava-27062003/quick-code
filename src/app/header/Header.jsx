f4bbcb2b35942aa0f1107cb8a8125d0ac3d6617b
Aishi Dutta <aishi.dutta@iemlabs.com>
2023-12-28 18:26:45 +0530

    button made

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div>
       <nav><img class="navimg" src="https://smartsociety.org/wp-content/uploads/elementor/thumbs/logo-q3mzwkonvhnqg0xhptv5otnaj3vpxq57ruywvmecq4.png" alt="" />
         <div class="navitems">
-          <a href="https://smartsociety.org/" class="navitems">Go To Home Page </a>
+          <button><a href="https://smartsociety.org/" class="navitems">Go To Home Page </a></button>
         </div>
     
     </nav>
